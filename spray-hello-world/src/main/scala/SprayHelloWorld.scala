6346c4c95804e0d0633e5439d46970bb4c7b2011
natemurthy <drip.dr0p.dr0p@gmail.com>
2015-08-16 21:21:45 -0700

    [spray-hello-world] compose actor message handlers

--- spray-hello-world/src/main/scala/SprayHelloWorld.scala
+++ spray-hello-world/src/main/scala/SprayHelloWorld.scala
@@ -1,28 +1,41 @@
 import akka.actor._
 import akka.io.IO
+import scala.concurrent.duration._
 import spray.can.Http
+import spray.http._
 import spray.http.MediaTypes._
 import spray.routing._
  
 object SprayHelloWorld extends App {
+  import java.net.InetAddress
   implicit val system = ActorSystem("spray-hello-world")
-  val actorRef = system.actorOf(Props[MyActor], "my-actor")
-  IO(Http) ! Http.Bind(actorRef, interface = "localhost", port = 8080)
+  val actorRef = system.actorOf(Props[MyActor], "handler")
+  //val actorRef = system.actorOf(Props[BenchmarkService], "handler")
+  IO(Http) ! Http.Bind(actorRef, interface = InetAddress.getLocalHost.getHostAddress, port = 8080)
 }
 
-class MyActor extends Actor with MyService {
+class MyActor extends Actor with HttpService {
+  import context.dispatcher
+  var count = 0
   def actorRefFactory = context
-  def receive = runRoute(routes)
-}
-
-trait MyService extends HttpService {
+  def receive = runRoute(routes) orElse otherMessages
   val routes =
+    path("hello") {
+      get {
+        respondWithMediaType(`application/json`) {
+          complete { """{"msg":"hello world"} """ }
+        }
+      }
+    } ~
     path("status") {
       get {
         respondWithMediaType(`application/json`) {
-          _.complete { """{"msg":"hello world"} """ }
+          complete { s""" {"count":$count, "uptime":${context.system.uptime}} """}
         }
       }
-    } 
+    }
+  def otherMessages: Receive = {
+    case "Tick" => count += 1
+  }
+  context.system.scheduler.schedule(0.seconds,2.seconds,self,"Tick")
 }
-
