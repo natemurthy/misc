b90b332a28c19040ae3c447a1edc69151e3ec678
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2015-09-12 21:04:16 -0700

    [oassg-orientdb] parse json from db records, mappers for vertices and edges

--- oassg-orientdb/src/main/scala/OrientDbWrapper.scala
+++ oassg-orientdb/src/main/scala/OrientDbWrapper.scala
@@ -2,6 +2,8 @@ import com.tinkerpop.blueprints._
 import com.tinkerpop.blueprints.impls.orient._
 import collection.JavaConversions._
 import javax.persistence.{Version, Id}
+import org.json4s._
+import org.json4s.jackson.JsonMethods
 import resource._
 
 object OrientDbWrapper {
@@ -9,20 +11,43 @@ object OrientDbWrapper {
   implicit def objectGraphMapper(graph: OrientGraph) = new ObjectGraphMapper(graph)
 
   final class ObjectGraphMapper(graph: OrientGraph) extends AnyVal {
-    def objectGraphMapper[T:TypeTag](id: String) = {
-      val getFromDb = (recordId:String) => {
-        graph.getVertices("record_id",recordId).toList.headOption
-          .getProperty("as_json").asInstanceOf[String]
+    
+    def objectVertexMapper[T:TypeTag](id: String) = {
+      implicit val readFormats = DefaultFormats
+      val getFromDb = 
+        graph.getVertices("record_id",id).toList.headOption
+          .map(_.getProperty("as_json").asInstanceOf[String])
+          .map(JsonMethods.parse)
       }
       typeOf[T] match {
-        case t if t =:= typeOf[Foo] => Some(t)
-        case t if t =:= typeOf[Bar] => Some(t)
+        case t if t =:= typeOf[Foo] => getFromDb.map(_.extract[Foo].asInstanceOf[T])
+        case t if t =:= typeOf[Bar] => getFromDb.map(_.extract[Bar].asInstanceOf[T])
         case _ => None
       }
     }
+    
+     def objectEdgeMapper[T:TypeTag](id: String) = {
+      implicit val readFormats = DefaultFormats
+      val getFromDb = 
+        graph.getEdges("record_id",id).toList.headOption
+          .map(_.getProperty("as_json").asInstanceOf[String])
+          .map(JsonMethods.parse)
+      }
+      typeOf[T] match {
+        case t if t =:= typeOf[Bippity] => getFromDb.map(_.extract[Bippity].asInstanceOf[T])
+        case t if t =:= typeOf[Boppity] => getFromDb.map(_.extract[Boppity].asInstanceOf[T])
+        case _ => None
+      }
+    }
+    
   }
 
 }
 
+// Vertex classes
 case class Foo(a:Int, b:String)
 case class Bar(color:String,price:Double)
+
+// Edge classes
+case class Bippity(x:Int,y:Int)
+case class Boppity(r1:String,r2:String)
