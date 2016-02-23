13290a42b3a70cf79327c4d87a6056bf58d9c5c9
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2016-02-23 00:27:55 -0800

    [interview-questions] fix typos and whitespace in question1

--- interview-questions/question1.scala
+++ interview-questions/question1.scala
@@ -2,8 +2,8 @@
 Quesiton 1
 
 Given a linked list of integers whose length is a multiple of k, implement a method
-that splits the linked list into sublists of length k, reverse each sublist, and then
-combine the sublists into a whole linked list. For example:
+that splits the linked list into sublists of length k, reverses each sublist, and then
+combines the sublists into a whole linked list. For example:
 
     input: 1 -> 2 -> 3 -> 4 -> 5 -> 6
    output: 3 -> 2 -> 1 -> 6 -> 5 -> 4
@@ -26,6 +26,6 @@ def reverseSplit(list: LinkedList[Int], k: Int): LinkedList[Int] = {
     list.reverse
   else {
     val splitList = list.splitAt(k)
-	splitList._1.reverse ++ reverseSplit(splitList._2,k)
+    splitList._1.reverse ++ reverseSplit(splitList._2,k)
   }
 }
