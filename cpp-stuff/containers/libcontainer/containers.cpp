339e6170d9e34b7d2a84d43739f7592cea0a4fbc
natemurthy <drip.dr0p.dr0p@gmail.com>
2016-07-07 00:03:19 -0700

    [cpp-stuff] implementation of container class

--- cpp-stuff/containers/libcontainer/containers.cpp
+++ cpp-stuff/containers/libcontainer/containers.cpp
@@ -1,29 +1,51 @@
 #include <stdlib.h>
-#include <list>
-#include <map>
-#include <memory>
-#include <set>
+#include <chrono>
+#include <iostream>
+#include <random>
 #include <string>
-#include <vector>
+#include <cstdlib>
 
 #include "containers.hpp"
 
+static std::string random_string(int length)
+{
+    static const std::string alphanums = "0123456789abcdef";
+
+    thread_local static std::mt19937 rg{std::random_device{}()};
+    thread_local static std::uniform_int_distribution<> pick(0, alphanums.size() - 1);
+
+    std::string s;
+
+    s.reserve(length);
+
+    while(length--)
+        s += alphanums[pick(rg)];
+
+    return s;
+}
+
 Container::Container()
 {
-  Container::__alive = true;
+  Container::status = Created;
+  Container::id = random_string(12);
 }
 
-bool Container::isAlive()
+std::string Container::getId()
 {
-  return Container::__alive;
+  return Container::id;
 }
 
-void Container::kill()
+Status Container::getStatus()
 {
-  Container::__alive = false;
+  return Container::status;
 }
 
-std::string Container::sayHello()
+void Container::start()
 {
-  return "Hello, I am a container";
+  Container::status = Running;
+}
+
+void Container::stop()
+{
+  Container::status = Stopped;
 }
