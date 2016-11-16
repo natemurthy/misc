ad44a822618b3bdb4b6ca85178e6fff46db27d82
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2016-11-16 01:00:37 -0800

    [interview-questions] minor edit to 15 Nov 2016 scala solution

--- interview-questions/15Nov2016/question1.scala
+++ interview-questions/15Nov2016/question1.scala
@@ -4,9 +4,10 @@
  *         move all zeros to tail
  **/
 
-var xs = List(1,2,3,-4,0,0,2,4,0)
+var xs = List(1, 2, 3, -4, 0, 0, 2, 4, 0)
 
 def moveZerosToTailAndCountNonZeros: Int = {
+  val nonZeros = xs.filter(_ != 0)
   xs = xs.filter(_ != 0) ++ xs.filter(_ == 0)
-  xs.filter(_ != 0).size
+  nonZeros.size
 }
