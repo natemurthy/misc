9e116fc16faa71d6fdfbbd799756d9d87eb68b25
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2016-11-16 01:01:28 -0800

    [interview-questions] minor edit to 15 Nov 2016 python solution

--- interview-questions/15Nov2016/question1.py
+++ interview-questions/15Nov2016/question1.py
@@ -4,7 +4,7 @@ output: count of non-zero integers
         move all zeros to tail of array
 """
 
-arr = [1,2,3,-4,0,0,2,4,0]
+arr = [1, 2, 3, -4, 0, 0, 2, 4, 0]
 
 def move_zeros_to_tail_and_count_nonzeros(arr):
     j = 0; k = len(arr)-1
