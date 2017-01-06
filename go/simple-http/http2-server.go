dcd7d9123644c107de26876394ce541700031e38
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2017-01-05 17:18:55 -0800

    [go] http2-server without TLS cert and key

--- go/simple-http/http2-server.go
+++ go/simple-http/http2-server.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "os"
+	"log"
+	"net/http"
 
-    "golang.org/x/net/http2"
+	"golang.org/x/net/http2"
 )
 
 func main() {
-    cwd, err := os.Getwd()
-    if err != nil {
-        log.Fatal(err)
-    }
+	// cwd, err := os.Getwd()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-    srv := &http.Server{
-        Addr:    ":8000", // Normally ":443"
-        Handler: http.FileServer(http.Dir(cwd)),
-    }
-    http2.ConfigureServer(srv, &http2.Server{})
-    log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
+	srv := &http.Server{
+		Addr: ":8000", // Normally ":443"
+		//Handler: http.FileServer(http.Dir(cwd)),
+	}
+	http2.ConfigureServer(srv, &http2.Server{})
+	log.Fatal(srv.ListenAndServe())
+	//log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
 }
