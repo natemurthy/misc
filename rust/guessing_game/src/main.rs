1e231517e08a8049f361ee2d2674a6c318a8ecc0
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2017-04-23 16:26:16 -0700

    [rust] getting started

---
+++ rust/guessing_game/src/main.rs
@@ -0,0 +1,22 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1,101);
+
+    println!("The secrete number is: {}", secret_number);
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
