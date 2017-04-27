12c5fa6da867ff06f0336aa63cb4687194f65cf7
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2017-04-27 12:06:08 -0700

    [go] mux-testing: encode json

--- go/mux-testing/main.go
+++ go/mux-testing/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "net/http"
 
     "github.com/gorilla/mux"
 )
 
+type JsValue struct {
+        Message string `json:"message"`
+}
+
 func GetRequest(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
     myString := vars["mystring"]
 
     w.WriteHeader(http.StatusOK)
-    w.Header().Set("Content-Type", "text/plain")
-    w.Write([]byte(myString))
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    json.NewEncoder(w).Encode(JsValue{Message: myString})
 }
 
 func NewRouter() *mux.Router {
