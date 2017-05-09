3f014a06868c5528bc408215a30e6e42cdacc23c
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2017-05-09 06:09:58 -0700

    [go] todo-rest-api: remove comments, adjust linespace

--- go/todo-rest-api/main_test.go
+++ go/todo-rest-api/main_test.go
@@ -10,19 +10,14 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
-// see http://stackoverflow.com/questions/34435185/unit-testing-for-functions-that-use-gorilla-mux-url-parameters
 func TestGetRequest(t *testing.T) {
-
     r, _ := http.NewRequest("GET", "/todos", nil)
     w := httptest.NewRecorder()
-
     NewRouter().ServeHTTP(w, r)
 
     var resp []Todo
     json.Unmarshal(w.Body.Bytes(), &resp)
-    //expect := JsValue{Message: "abcd"}
-    fmt.Println(resp)
+
     assert.Equal(t, http.StatusOK, w.Code)
     assert.Equal(t, len(resp),     2)
-    //assert.Equal(t, expect,        resp  )
 }
