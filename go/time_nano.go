5312a5dbfba109e7d6b26a361805c18492ddad69
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2017-07-18 12:11:28 -0700

    [go] time_nano.go: example of parsing/rendering with ISO 8601

--- go/time_nano.go
+++ go/time_nano.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	// incrementing timestamp
         var now = time.Now().UnixNano()
         var foo = 1491198158145595191
         fmt.Println(foo)
@@ -13,5 +14,19 @@ func main() {
         fmt.Println(foo)
         fmt.Println(now)
 	fmt.Println(now + 1000000000)
+	
+	//render
+        dtStr := now.Format(time.RFC3339)
+        fmt.Println(dtStr)
+
+        //parse
+        s := dtStr // "2017-07-18T18:50:20.123Z"
+        dtTime, err := time.Parse(time.RFC3339, s)
+        if err != nil {
+                fmt.Println(err)
+                return
+        } else {
+                fmt.Println(dtTime.UnixNano())
+        }
 }
 
