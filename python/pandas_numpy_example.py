251f997d95652cea1a7c41c84428d30d4de14ab1
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2017-08-25 11:16:13 -0700

    [python] update pandas_numpy_example.py

--- python/pandas_numpy_example.py
+++ python/pandas_numpy_example.py
@@ -1,10 +1,9 @@
+# See https://stackoverflow.com/questions/32752292/pandas-how-to-create-a-data-frame-of-random-integers
+# And https://stackoverflow.com/questions/12137277/how-can-i-make-a-python-numpy-arange-of-datetime
+
 import numpy as np
 import pandas as pd
 import datetime as dt
-
-# 
-# See https://stackoverflow.com/questions/32752292/pandas-how-to-create-a-data-frame-of-random-integers
-# And https://stackoverflow.com/questions/12137277/how-can-i-make-a-python-numpy-arange-of-datetime
 
 N = 10
 df = pd.DataFrame(np.random.randn(N, 4), columns=list('ABCD'))
