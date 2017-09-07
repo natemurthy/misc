fa3c728a85e127851bd5825bdbeccf4790c3ef95
natemurthy <drip.dr0p.dr0p@gmail.com>
2017-09-07 13:14:04 -0700

    [grpc] add python scripts to python-grpc-gateway-example

---
+++ grpc/python-grpc-gateway-example/client.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+import grpc
+
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+def run():
+    channel = grpc.insecure_channel('localhost:9090')
+    stub = helloworld_pb2_grpc.GreeterStub(channel)
+    response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+    print("Greeter client received: " + response.message)
+
+
+if __name__ == '__main__':
+    run()
