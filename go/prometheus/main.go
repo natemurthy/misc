c6c0aaeb0ae90ea57b9ccf763b8d4656262f9577
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2017-09-23 17:30:47 -0700

    [go] simple promehteus http metrics endpoint example

---
+++ go/prometheus/main.go
@@ -0,0 +1,18 @@
+// A minimal example of how to include Prometheus instrumentation.
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+
+func main() {
+	flag.Parse()
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
