e532e8e6868f227b438489db9a92aab477b5b7bd
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2018-03-04 12:10:40 -0800

    [algomusic] pipe output from xxd to stdout

--- algomusic/main.go
+++ algomusic/main.go
@@ -1,36 +1,24 @@
-// Copyright 2017 Hajime Hoshi
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// +build example
-
 package main
 
+// Borrowed from: https://github.com/hajimehoshi/go-mp3/blob/master/example/main.go
+
 import (
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/hajimehoshi/go-mp3"
 	"github.com/hajimehoshi/oto"
 )
 
-func run() error {
-	f, err := os.Open("input.mp3")
-	if err != nil {
-		return err
-	}
-	defer f.Close()
+var (
+	input = flag.String("input", "input.mp3", "name of mp3 file")
+)
+
+func play(f *os.File) error {
 
 	d, err := mp3.NewDecoder(f)
 	if err != nil {
@@ -49,12 +37,35 @@ func run() error {
 	if _, err := io.Copy(p, d); err != nil {
 		return err
 	}
+
 	return nil
 }
 
 func main() {
+	flag.Parse()
+
+	f, err := os.Open(*input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	go func() {
+		for i := 47; i < 55000; i = i + 50 {
+			window := fmt.Sprintf("%d,%dp", i, i+50)
+			c1 := exec.Command("xxd", "-b", "-c", "9", *input)
+			c2 := exec.Command("sed", "-n", window)
+			c2.Stdin, _ = c1.StdoutPipe()
+			c2.Stdout = os.Stdout
+			_ = c2.Start()
+			_ = c1.Run()
+			_ = c2.Wait()
+		}
+
+	}()
+
 	for {
-		if err := run(); err != nil {
+		if err := play(f); err != nil {
 			log.Fatal(err)
 		}
 	}
