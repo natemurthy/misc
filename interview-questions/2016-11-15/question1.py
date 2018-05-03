fc08b725b7949ce50e9efd467776c4d8a6438546
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2018-05-02 18:30:19 -0700

    append docstring with link to cleaner solution

--- interview-questions/2016-11-15/question1.py
+++ interview-questions/2016-11-15/question1.py
@@ -4,24 +4,61 @@ output: count of non-zero integers
         move all zeros to tail of array
 """
 
-arr = [1, 2, 3, -4, 0, 0, 2, 4, 0]
 
 def move_zeros_to_tail_and_count_nonzeros(arr):
-    j = 0; k = len(arr)-1
+    j = 0; end = len(arr)-1; k = end
     for i,v in enumerate(arr):
-        if i < k and v == 0:
-            try: 
-                while arr[k] == 0:
-                    j += 1
-                    k = len(arr)-j-1
-            except IndexError:
-                return 0
-            temp = arr[k]
-            arr[k] = v
-            arr[i] = temp
-            j += 1
-            k = len(arr)-j-1
+        #print i, arr
+        if i <= k and v == 0:
+            while arr[k] == 0:
+                j += 1
+                k = end-j
+                if k < 1:
+                    break
+            #print '    ', i, j, k, arr[k], arr
+            if i <= k:
+                temp = arr[k]
+                arr[k] = v
+                arr[i] = temp
+                j += 1
+                k = end-j
         else:
             continue
     return k+1
-         
+
+
+arr = [1, 2, 3, -4, 0, 0, 2, 4, 0]
+print move_zeros_to_tail_and_count_nonzeros(arr)  == 6
+print arr == [1, 2, 3, -4, 4, 2, 0, 0, 0]
+
+arr = [1]
+print move_zeros_to_tail_and_count_nonzeros(arr)  == 1
+print arr == arr
+
+arr = [0,0,0]
+print move_zeros_to_tail_and_count_nonzeros(arr)  == 0
+print arr == [0,0,0]
+
+arr = [1,2,3,0,0]
+print move_zeros_to_tail_and_count_nonzeros(arr)  == 3
+print arr == [1,2,3,0,0]
+
+arr = [0,0,1,2,3]
+print move_zeros_to_tail_and_count_nonzeros(arr)  == 3
+print arr == [3,2,1,0,0]
+
+arr = [1,0,1,0,1,0]
+print move_zeros_to_tail_and_count_nonzeros(arr)  == 3
+print arr == [1,1,1,0,0,0]
+
+arr = [1,0,1,0,1]
+print move_zeros_to_tail_and_count_nonzeros(arr)  == 3
+print arr == [1,1,1,0,0]
+
+arr = [0,1,0,1,0,1]
+print move_zeros_to_tail_and_count_nonzeros(arr)  == 3
+print arr == [1,1,1,0,0,0]
+
+arr = [0,1,0,1,0,1,0]
+print move_zeros_to_tail_and_count_nonzeros(arr)  == 3
+print arr == [1,1,1,0,0,0,0]
