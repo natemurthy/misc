2c7e0ddfb12a3c42364c44237d5f8032ee0ed5bd
Nathan Murthy <drip.dr0p.dr0p@gmail.com>
2018-08-15 02:32:36 -0700

    [go] github.com/dave/jennifer example

---
+++ go/codegen/jennifer.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	. "github.com/dave/jennifer/jen"
+)
+
+func main() {
+	f := NewFile("main")
+	f.Func().Id("main").Params().Block(
+		Qual("fmt", "Println").Call(Lit("Hello, world")),
+	)
+	fmt.Printf("%#v", f)
+	//f.Save("temp.go")
+}
