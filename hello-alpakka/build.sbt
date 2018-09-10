bf27b142a89c05fd772410f36e51fc7a4e6550b5
natemurthy <drip.dr0p.dr0p@gmail.com>
2018-09-10 14:49:51 -0700

    [hello-alpakka] cleaner logging and env var config input

--- hello-alpakka/build.sbt
+++ hello-alpakka/build.sbt
@@ -5,6 +5,15 @@ scalaVersion := "2.12.6"
 sbtVersion := "1.2.1"
 
 libraryDependencies ++= Seq(
+  "ch.qos.logback"    %  "logback-classic"   % "1.2.3",
+  "com.typesafe"      %  "config"            % "1.3.1",
+  "com.typesafe.akka" %% "akka-slf4j"        % "2.5.16",
   "com.typesafe.akka" %% "akka-stream-kafka" % "0.22"
 )
 
+assemblyMergeStrategy in assembly := {
+  case PathList("reference.conf") => MergeStrategy.concat
+  case PathList("scalastyle-config.xml", xs @ _*) => MergeStrategy.discard
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => (assemblyMergeStrategy in assembly).value(x)
+}
--- hello-alpakka/src/main/scala/HelloAlpakkaConsumer.scala
+++ hello-alpakka/src/main/scala/HelloAlpakkaConsumer.scala
@@ -3,7 +3,9 @@ import akka.kafka.{ConsumerSettings, Subscriptions}
 import akka.kafka.scaladsl.Consumer
 import akka.stream.scaladsl.{Keep, Sink}
 import akka.stream.{ActorMaterializer, Materializer}
+import com.typesafe.config.{ConfigFactory,ConfigValueFactory}
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.util.{Failure, Success}
@@ -14,31 +16,33 @@ import scala.util.{Failure, Success}
   */
 object HelloAlpakkaConsumer extends App {
 
-  println("Hello from Alpakka")
+  private[this] val log = LoggerFactory.getLogger(getClass.getName.stripSuffix("$"))
+  private[this] val config = ConfigFactory.load()
+  private[this] val topic = config.getString("akka.kafka.consumer.kafka-clients.topic")
+
+  log.info("Hello from Alpakka")
 
   implicit val system: ActorSystem = ActorSystem("hello-alpakka")
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
   implicit val materializer: Materializer = ActorMaterializer()
 
-  val consumerSettings =
-    ConsumerSettings(system, new StringDeserializer, new StringDeserializer)
-      .withBootstrapServers(args.head)
+  val settings = ConsumerSettings(system, new StringDeserializer, new StringDeserializer)
 
   val consumer = Consumer
-    .plainSource(consumerSettings, Subscriptions.topics("streams-plaintext-input"))
+    .plainSource(settings, Subscriptions.topics(topic))
     .toMat(Sink.foreach(r => println(r.value)))(Keep.both)
     .mapMaterializedValue(Consumer.DrainingControl.apply)
     .run()
 
   sys.addShutdownHook({
-    println("Shutdown requested...")
+    log.info("Shutdown requested...")
 
     val done = consumer.shutdown()
 
-    implicit val ec: ExecutionContextExecutor = system.dispatcher
     done
       .onComplete {
-        case Success(_) => println("Done"); system.terminate()
-        case Failure(err) => println(err.toString); system.terminate()
+        case Success(_) => log.info("Done"); system.terminate()
+        case Failure(err) => log.error(err.toString); system.terminate()
       }
   })
 
