a732b89acce4c907389474f6d3b6f544ea42d2b9
natemurthy <drip.dr0p.dr0p@gmail.com>
2018-09-27 13:13:42 -0700

    [go] github.com/jessevdk/go-flags example

---
+++ go/go_flags_example.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	flags "github.com/jessevdk/go-flags"
+	"github.com/k0kubun/pp"
+)
+
+// Config defines all env vars and flags
+type Config struct {
+	Foo          string `env:"FOO" long:"foo" short:"f" description:"you totally don't need this"`
+	KafkaEnabled bool   `env:"KAFKA" long:"enable-kafka" description:"enable logging messages to kafka"`
+}
+
+func main() {
+	var config = Config{}
+	flagParser := flags.NewParser(&config, flags.HelpFlag|flags.PrintErrors)
+	flagParser.NamespaceDelimiter = "-"
+	flagParser.Command.Name = "foo"
+	flagParser.Usage = "{ --my-server wss://HOST:443/ --my-cert... | --message-file=FILE.yaml } [ OPTIONS ]"
+	_, err := flagParser.Parse()
+	if err != nil {
+		if err, ok := err.(*flags.Error); ok && (err.Type == flags.ErrHelp) {
+			return
+		}
+		os.Exit(2)
+	}
+	pp.Println(config)
+}
