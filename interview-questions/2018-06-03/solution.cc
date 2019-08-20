eeadd7387096c2fdcc30debfe06d4ed691157b7b
Nathan Murthy <1788878+natemurthy@users.noreply.github.com>
2019-08-20 16:41:18 -0700

    [interview-questions] cleaner C++ code

--- interview-questions/2018-06-03/solution.cc
+++ interview-questions/2018-06-03/solution.cc
@@ -8,26 +8,16 @@
 
 using namespace std;
 
-bool HasPairWithSum(const vector<int>& data, int sum)
-{
+bool HasPairWithSum(const vector<int>& data, int sum) {
         unordered_set<int> comp; // additive complements
-        for (int value : data) 
-        {
+        for (int value : data) {
                 if (comp.find(value) != comp.end()) return true;
                 comp.insert(sum-value);
         }
         return false;
 }
 
-int main()
-{
-        int myints1[] = {1,2,4,9};
-        vector<int> input1 (myints1, myints1 + sizeof(myints1) / sizeof(int) );
-        cout << HasPairWithSum(input1, 8) << endl;
-
-        int myints2[] = {1,2,4,4};
-        vector<int> input2 (myints2, myints2 + sizeof(myints2) / sizeof(int) );
-        cout << HasPairWithSum(input2, 8) << endl;
-
-        return 0;
+int main() {
+        cout << HasPairWithSum({1,2,4,9}, 8) << endl;
+        cout << HasPairWithSum({1,2,4,4}, 8) << endl;
 }
