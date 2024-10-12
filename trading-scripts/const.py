8b36802cee990f6a9c2b2862e22afcddcf30d24d
natemurthy <1788878+natemurthy@users.noreply.github.com>
2024-10-12 08:05:07 -0700

    [trading-scripts] const

---
+++ trading-scripts/const.py
@@ -0,0 +1,52 @@
+import pytz
+
+TZ_US_PACIFIC = pytz.timezone("America/Los_Angeles")
+
+MY_SORTED_SYMBOLS = "my_sorted_symbols.txt"
+
+ETF_SYMBOLS = [
+    "AFK",
+    "AGGH",
+    "ARKK",
+    "ARKX",
+    "BITO",
+    "BUCK",
+    "CDX",
+    "COWZ",
+    "CTA",
+    "DIA",
+    "EQLS",
+    "ESGU",
+    "EWJ",
+    "FAN",
+    "FFND",
+    "FLOT",
+    "GK",
+    "GLD",
+    "GOVT",
+    "HEQT",
+    "HIGH",
+    "ILF",
+    "INDA",
+    "IWM",
+    "JEPI",
+    "JETS",
+    "KOMP",
+    "KRE",
+    "MCHI",
+    "MJ",
+    "PFF",
+    "QQQ",
+    "SPY",
+    "SVOL",
+    "TAN",
+    "TLT",
+    "VOO",
+]
+
+USER_AGENT_HEADERS = [
+    {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0'},
+    {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'},
+    {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'},
+]
+
